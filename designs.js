a1c22962c0158f95a8456be8ac4e993eda8a660d
Mohamed Ismail <mreda.cs@gmail.com>
2017-11-27 22:52:16 +0300

    feat: getting input data and calling makeGrid when size submitted

--- designs.js
+++ designs.js
@@ -1,10 +1,25 @@
+
 // Select color input
+var color = $("#colorPicker").val();
+
 // Select size input
+var input_height = $("#input_height").val();
+var input_width = $("#input_width").val();
+
 
 // When size is submitted by the user, call makeGrid()
+$(document).ready(function() {
+	$("#sizePicker input:submit").on("click", makeGrid);
+});
 
 function makeGrid() {
+	input_height = $("#input_height").val();
+	input_width = $("#input_width").val();
+	color = $("#colorPicker").val();
 
-// Your code goes here!
-
+	// Your code goes here!
+	alert("draw grid with size of ( " + input_height + ", " + input_width + ") and intial color is " + color);
 }
+
+
+
